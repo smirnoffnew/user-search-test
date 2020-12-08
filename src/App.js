2d594e34b4ebabc1a91e9b71fe4c657eac74033d
smirnov.ihor <smirnoffnew@gmail.com>
2020-12-08 10:40:25 +0200

    feat: get user list data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import UserListModel from './models/users';
 
-function App() {
-  return (
-    <div className="App">
+const App = () => {
+	const [ list, setList ] = useState(null);
 
-    </div>
-  );
-}
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/users').then((response) => response.json()).then((json) => {
+			setList((json || []).map(UserListModel));
+		});
+	}, []);
+
+	return (
+		<div className="App">
+			<div className="title">Users list</div>
+		</div>
+	);
+};
 
 export default App;
---
+++ src/models/users.js
@@ -0,0 +1,9 @@
+const UserListModel = (data) => {
+    return {
+		id: data.id,
+		name: data.name,
+		username: data.username
+	};
+};
+
+export default UserListModel;
